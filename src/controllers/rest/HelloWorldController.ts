a4d0890c8b2aae35f003baaeffd68a6b0027f6ab
Nick <nicholas.antolos@snhu.edu>
2024-10-17 16:32:11 -0400

    chore: biome format

--- src/controllers/rest/HelloWorldController.ts
+++ src/controllers/rest/HelloWorldController.ts
@@ -1,10 +1,10 @@
-import {Controller} from "@tsed/di";
-import {Get} from "@tsed/schema";
+import { Controller } from "@tsed/di";
+import { Get } from "@tsed/schema";
 
 @Controller("/sync")
 export class HelloWorldController {
-  @Get("/")
-  get() {
-    return "Totally just synced the database for real for real, totally not just a string return..";
-  }
+	@Get("/")
+	get() {
+		return "Hello World";
+	}
 }
