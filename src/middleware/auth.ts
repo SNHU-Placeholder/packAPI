8bff7cef34707e939853ea96793c820e5fb7f62e
avery b <averwhy@gmail.com>
2024-11-04 15:46:26 -0500

    Update auth middleware to use json

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,9 @@ import type { RequestHandler } from "express";
 
 export const auth: RequestHandler = async (req, res, next) => {
     if (req.headers.authorization !== "Bearer 123") {
-        res.status(401).send("Unauthorized");
+        res.status(401).json(
+            {"response": "Unauthorized"}
+        );
         return;
     }
 
